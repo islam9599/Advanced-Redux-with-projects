7ece165258a405927d057923b3405ec038b120dd
islam9599 <eislombek9599@gmail.com>
2023-03-23 14:00:40 +0900

    Update!

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { Fragment } from "react";
+import { useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 
 function App() {
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
   return (
     <Fragment>
       <Layout>
-        <Cart />
+        {showCart && <Cart />}
         <Products />
       </Layout>
     </Fragment>
--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,46 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+const dummyProducts = [
+  {
+    id: "p1",
+    price: 6,
+    title: "My 1st book!",
+    description: "My first book i ever wrote",
+  },
+  {
+    id: "p2",
+    price: 5,
+    title: "My 2nd book!",
+    description: "My second book i ever wrote",
+  },
+  {
+    id: "p3",
+    price: 7,
+    title: "My 3rd book!",
+    description: "My third book i ever wrote",
+  },
+  {
+    id: "p4",
+    price: 9,
+    title: "My 4th book!",
+    description: "My fourth book i ever wrote",
+  },
+];
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {dummyProducts.map((product) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
